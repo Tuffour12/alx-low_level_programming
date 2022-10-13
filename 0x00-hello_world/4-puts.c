963cf9d7cd4f025300019920cccd5e6d8ac016c1
Tuffour12 <solomonalx38@gmail.com>
2022-10-13 16:44:13 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - Printing without using printif
- *
- * Return: Always 0 
+ * Return -Always 0
  */
-int main(void) 
+int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle"); 
-	
-	return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main:Printif
- *
  * Return : 0 
  */
-{int main(void)
-
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0); 
-}}
+}
