1d3ab79454abcad5268dc81dc7c0f42f3e7e9803
Tuffour12 <solomonalx38@gmail.com>
2022-10-13 15:03:56 -0700

    Puts function update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+/**
+ * main - Printing without using printif
+ *
+ * Return: Always 0 
+ */
+
+int main(void) 
+{ 
+	puts("Programming is like building a multilingual puzzle\n"); 
+	
+	return (0); 
+}
