5cf867be5f360c01fabe52b6dd77fc2fe596c98f
Tuffour12 <solomonalx38@gmail.com>
2022-10-13 15:07:18 -0700

    Puts function update 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,4 @@
  *
  * Return: Always 0 
  */
-
-int main(void) 
-{ 
-	puts("Programming is like building a multilingual puzzle\n"); 
-	
-	return (0); 
-}
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
