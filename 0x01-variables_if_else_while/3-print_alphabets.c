43093ad18457a9627be85174db4bcbfb948c72a5
Tuffour12 <solomonalx38@gmail.com>
2022-10-14 21:12:37 -0700

    Putchar both lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	char uc;
 	
-	for (uc = 'a'; uc <= 'z; uc++)
-	{
+	for (uc = 'a'; uc <= 'z'; uc++)
+	
 		putchar(uc);
-	}
+	
 
 	for (uc = 'A'; uc <= 'Z'; uc++)
-	{
+	
 		putchar(uc);
-	}
+
 	putchar('\n');
 	return (0)
 }
