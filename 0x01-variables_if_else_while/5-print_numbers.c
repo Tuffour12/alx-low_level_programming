51dbc6be2efb71ffe89aab5fdfafcbc8513cfe95
Tuffour12 <solomonalx38@gmail.com>
2022-10-14 21:52:52 -0700

    Print form zerp to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Write a code that print a number in base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf(n);
+	}
+	printf('\n');
+
+	return (0);
+}
