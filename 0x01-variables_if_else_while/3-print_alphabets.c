cde8840acc2cf93d8985681da1dcd77ba1048aeb
Tuffour12 <solomonalx38@gmail.com>
2022-10-14 20:53:42 -0700

    Print both lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print letter in upper case
+ *
+ * Return: Alway 0
+ */
+int main(void)
+{
+	char uc;
+
+	for (uc = 'A'; uc <= 'Z'; uc++)
+
+		putchar(uc);
+
+	for (uc = 'a'; uc <= 'z; uc++)
+
+		putchar(uc);
+	putchar('\n');
+	return (0)
+}
