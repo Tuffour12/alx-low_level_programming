8a681861da9feb364637a643e48ebadd64f7f2b9
Tuffour12 <solomonalx38@gmail.com>
2022-10-14 21:05:27 -0700

    Putchar two

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,14 @@ int main(void)
 	char uc;
 	
 	for (uc = 'a'; uc <= 'z; uc++)
-	
+	{
 		putchar(uc);
-	
+	}
+
 	for (uc = 'A'; uc <= 'Z'; uc++)
-
+	{
 		putchar(uc);
-
+	}
 	putchar('\n');
 	return (0)
 }
