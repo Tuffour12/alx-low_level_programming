eb1181b8a873cc276800afed3d284fcf47c330cc
Tuffour12 <solomonalx38@gmail.com>
2022-10-14 22:49:47 -0700

    Task zero commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,11 +9,13 @@
  * Return: Always 0
  */
 int main(void)
-{	
+{
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -23,8 +25,8 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	else
-	{			
+	{
 		printf("%d is negative\n", n);
 	}
 	return (0);
-}	
+}
