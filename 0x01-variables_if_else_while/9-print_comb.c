603197a0d647b783533b6f6983ea8a027bf84ad6
Tuffour12 <solomonalx38@gmail.com>
2022-10-14 23:45:59 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,12 @@ int main(void)
 	int n;
 
 	for (n = 48; n < 58; n++)
-		putchar(n);
+	{	putchar(n);
 		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	
 	putchar('\n');
 }	return (0);
