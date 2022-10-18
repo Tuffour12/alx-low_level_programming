37d66ce7e089f92f9d725dcecbfff94fbee355e7
Tuffour12 <solomonalx38@gmail.com>
2022-10-18 01:49:53 -0700

    Print putchsr

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+	return (0);
+}	
