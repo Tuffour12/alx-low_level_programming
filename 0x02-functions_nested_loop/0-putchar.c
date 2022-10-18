752c63c65210520dc45e1df76e8ff1cda737f606
Tuffour12 <solomonalx38@gmail.com>
2022-10-18 14:04:42 -0700

    Putchar command

--- 0x02-functions_nested_loop/0-putchar.c
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -1,14 +1,21 @@
-#include <stdio.>
+#include "main.h"
 /**
  * main - Entry point
  *
- * Description: Program that prints _putchar
+ * Description: Print putchar and a new line
  *
  * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar");
-	printf("\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
-}	
+}
