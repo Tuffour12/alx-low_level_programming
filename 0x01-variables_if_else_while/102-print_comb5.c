e2caf06cfd81abb5f156272699daf49e9d45ee23
Tuffour12 <solomonalx38@gmail.com>
2022-10-17 18:58:15 -0700

    Combin 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:  possible combinations of two two-digit number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numb1, numb2;
+
+	for (numb1 = 0; numb1 <= 98; numb1++)
+	{
+		for (numb2 = numb1 + 1; numb2 <= 99; numb2++)
+		{
+			putchar((numb1 / 10) + '0');
+
+			putchar((numb1 % 10) + '0');
+
+			putchar(' ');
+
+			putchar((numb2 / 10) + '0');
+
+			putchar((numb2 % 10) + '0');
+			if (numb1 == 98 && numb2 == 99)
+				continue;
+			putchar(',');
+
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
