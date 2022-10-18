1d8ed0cd800303ed00bb0b6b4922b7e470460c25
Tuffour12 <solomonalx38@gmail.com>
2022-10-17 18:44:58 -0700

    Comb four

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	int x, y, z;
+
 	for (x = 0; x < 8; x++)
 	{
 		for (y = x + 1; y < 9; y++)
-		{	
+		{
 			for (z = y + 1; z < 10; z++)
 			{
 				putchar((x % 10) + '0');
@@ -27,7 +28,7 @@ int main(void)
 				putchar(' ');
 
 			}
-		}	
+		}
 
 	}
 	putchar('\n');
