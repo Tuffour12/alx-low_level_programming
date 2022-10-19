c98878c975174eeaabfcf9c51bee4c31ad241133
Tuffour12 <solomonalx38@gmail.com>
2022-10-19 08:21:08 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,13 @@ void print_alphabet(void)
 {
 	char lc;
 
-	for (lc = "a"; lc <= "z"; lc++)
-		_putchar(lc);
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{	_putchar(lc);
+
+	}	
 
 
 
-	_putchar("\n");
+	_putchar('\n');
 
 }
