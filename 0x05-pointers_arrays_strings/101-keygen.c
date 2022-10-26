8dbdb931db9203f29b71a933867a04735fe5cd4f
Tuffour12 <solomonalx38@gmail.com>
2022-10-25 21:02:54 -0700

    Task 100

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int pass[100];
+
+	int q, sum, x;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (q = 0; q < 100; q++)
+	{	pass[q] =rand() % 78;
+	
+		sum += (pass[q] + '0');
+		putchar(pass[q] + '0');
+	
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}	
