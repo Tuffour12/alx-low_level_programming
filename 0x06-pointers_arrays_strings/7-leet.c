8584ede568371683adeab34ec165412e17ab4c38
Tuffour12 <solomonalx38@gmail.com>
2022-10-28 23:51:10 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * One if, Two loops only
+ * @x: input
+ * Return: Always 0 (Success)
+ */
+
+char *leet(char *x)
+{
+	int i, v;
+
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	int replacer[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; v[i] != '\0'; i++)
+	{
+		for (v = 0; v <= 9; v++)
+
+		{
+			if (x[i] == find[v])
+			{
+				x[i] = replacer[x / 2];
+				v = 9;
+			}
+		}
+
+	}
+	return (x);
+
+}
