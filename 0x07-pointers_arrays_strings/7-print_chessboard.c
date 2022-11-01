5621eb76a84c18249f3468ab73b10211e553609a
Tuffour12 <solomonalx38@gmail.com>
2022-11-01 13:33:06 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, x;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (x = 0; x < 8; x++)
+			_putchar(a[i][x]);
+		_putchar('\n');
+	}
+	return (0)
+}
